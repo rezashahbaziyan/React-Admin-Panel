9a2bf3138b7e422b2ea75b8b189deb4cd374462d
ujin <rezashahtrader@gmail.com>
2023-06-13 18:46:57 +0330

    .

--- src/App.js
+++ src/App.js
@@ -6,14 +6,33 @@ import Login from './Pages/login/Login';
 import New from './Pages/New/New';
 import Single from './Pages/Single/Single';
 
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from 'react-router-dom';
+
 const App = () => {
     return (
         <div className='App'>
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/'>
+                        <Route index element={<Home />} />
+                        <Route path='Login' element={<Login />} />
+                        <Route path='users'>
+                            <Route index element={<List />} />
+                            <Route path=':userId' element={<Single />} />
+                            <Route path='new' element={<New />} />
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+            {/* <Home />
             <List />
             <Login />
             <New />
-            <Single />
+            <Single /> */}
         </div>
     )
 }
