3b64cfeaddef38e30a21e4d0d6d8decc21c7c5c6
ujin <rezashahtrader@gmail.com>
2023-06-13 18:20:13 +0330

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./App.css";
 import Home from './Pages/Home/Home';
 import List from './Pages/list/List';
+import Login from './Pages/login/Login';
 
 const App = () => {
     return (
         <div className='App'>
             <Home />
             <List />
+            <Login />
         </div>
     )
 }
---
+++ src/Pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Login.scss";
+
+const Login = () => {
+  return (
+    <div className="Login">
+        <h1 className="title">Login</h1>
+    </div>
+  )
+}
+
+export default Login
