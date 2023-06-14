08e8eca66d6f501cf9c71a0602537cb9ffcb46b3
ujin <rezashahtrader@gmail.com>
2023-06-14 18:45:09 +0330

    .

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -3,7 +3,24 @@ import "./Sidebar.scss";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="Sidebar">
+        <div className='top'><span className="logo">lamadmin</span></div>
+        <div className='center'>
+            
+            <ul>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+            </ul>
+        </div>
+        <div className='bottom'>color options</div>
+    </div>
   )
 }
 
