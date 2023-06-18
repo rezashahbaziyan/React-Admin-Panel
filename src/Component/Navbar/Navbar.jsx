b1cde2d112949391292f34e0f7c85ede2c720632
ujin <rezashahtrader@gmail.com>
2023-06-18 17:22:53 +0330

    .

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -15,24 +15,24 @@ const Navbar = () => {
       <div className="wrapper">
         <div className='search'>
           <input type="text" placeholder='Search...' />
-          <SearchOutlinedIcon/>
+          <SearchOutlinedIcon className='icon'/>
         </div>
         <div className="items">
           <div className="item">
-            <LanguageOutlinedIcon/>
+            <LanguageOutlinedIcon className='icon'/>
             English
           </div>
           <div className="item">
-            <DarkModeOutlinedIcon/>
+            <DarkModeOutlinedIcon className='icon'/>
           </div>
           <div className="item">
-            <FullscreenExitOutlinedIcon/>
+            <FullscreenExitOutlinedIcon className='icon'/>
           </div>
           <div className="item">
-            <NotificationsNoneOutlinedIcon/>
+            <NotificationsNoneOutlinedIcon className='icon'/>
           </div>
           <div className="item">
-            <ListOutlinedIcon/>
+            <ListOutlinedIcon className='icon'/>
           </div>
         </div>
       </div>
