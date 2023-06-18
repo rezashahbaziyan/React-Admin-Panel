f7c47d754aa2ec4f9158dbbeda95a1aee13d2692
ujin <rezashahtrader@gmail.com>
2023-06-18 17:35:09 +0330

    .

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import ListOutlinedIcon from '@mui/icons-material/ListAltOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import avatar from "../../img/92777471.jpg";
 
 
@@ -30,6 +31,11 @@ const Navbar = () => {
           </div>
           <div className="item">
             <NotificationsNoneOutlinedIcon className='icon'/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon'/>
+            <div className="counter">2</div>
           </div>
           <div className="item">
             <ListOutlinedIcon className='icon'/>
