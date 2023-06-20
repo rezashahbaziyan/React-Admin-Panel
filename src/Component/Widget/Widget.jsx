e5f7d13bcb44591f0c8b61bab2d4a673519b7bd3
ujin <rezashahtrader@gmail.com>
2023-06-20 06:33:43 +0330

    .

--- src/Component/Widget/Widget.jsx
+++ src/Component/Widget/Widget.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import "./Widget.scss";
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined'; 
 
 
 
 const Widget = ({type}) => {
   let data;
+
+  const amount = 100;
+  const diff = 20;
 
   switch (type) {
     case "user":
@@ -15,7 +20,63 @@ const Widget = ({type}) => {
             title:"USERS",
             isMoney: false,
             link:"See all users",
-            icon: <PersonOutlinedIcon className='icon'/>,
+            icon: (
+                <PersonOutlinedIcon 
+                    className='icon' 
+                    style={{
+                        color:"crimson",
+                        backgroundColor:"rgba(255,0,0,0.2)"
+                    }}
+                />
+            ),
+        };
+        break;
+    case "order":
+        data={
+            title:"ORDERS",
+            isMoney: false,
+            link:"View all orders",
+            icon: (
+                <ShoppingCartOutlinedIcon 
+                    className='icon' 
+                    style={{
+                        color:"goldenrod",
+                        backgroundColor:"rgba(218,165,32,0.2)"
+                    }}
+                />
+            ),
+        };
+        break;
+    case "earning":
+        data={
+            title:"EARNINGS",
+            isMoney: true,
+            link:"View net earnings",
+            icon: (
+                <MonetizationOnOutlinedIcon 
+                    className='icon' 
+                    style={{
+                        color:"green",
+                        backgroundColor:"rgba(0,128,0,0.2)"
+                    }}
+                />
+            ), 
+        };
+        break;
+    case "balance":
+        data={
+            title:"BALANCE",
+            isMoney: true,
+            link:"See details",
+            icon: (
+                <AccountBalanceWalletOutlinedIcon 
+                    className='icon' 
+                    style={{
+                        color:"purple",
+                        backgroundColor:"rgba(128,0,128,0.2)"
+                    }}
+                />
+            ),
         };
         break;
       default:
@@ -24,16 +85,16 @@ const Widget = ({type}) => {
   return (
     <div className='widget'>
         <div className="left">
-            <span className="title">USERS</span>
-            <span className="counter">21312</span>
-            <span className="link">See all user</span>
+            <span className="title">{data.title}</span>
+            <span className="counter">{data.isMoney && "$"}{amount}</span>
+            <span className="link">{data.link}</span>
         </div>
         <div className="right">
             <div className="percentage positive">
                 <KeyboardArrowUpIcon/>
-                20 %
+                {diff} %
             </div>
-            <PersonOutlinedIcon className='icon'/>
+            {data.icon}
         </div>
     </div>
   )
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <div className="widgets">
               <Widget type="user"/>
               <Widget type="order"/>
-              <Widget type="erarning"/>
+              <Widget type="earning"/>
               <Widget type="balance"/>
             </div>
           </div>
