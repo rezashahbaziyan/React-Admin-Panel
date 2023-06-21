f907f1dc3e695f55cab60ada95ce4bc6781ce5fe
ujin <rezashahtrader@gmail.com>
2023-06-21 16:30:20 +0330

    .

--- src/Component/Featured/Featured.jsx
+++ src/Component/Featured/Featured.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import "./Featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+
 
 const Featured = () => {
   return (
-    <div className='featured'>Featured</div>
+    <div className='featured'>
+        <div className="top">
+            <h1 className="title">Total Revenue</h1>
+            <MoreVertIcon fontSize='small'/>
+        </div>
+        <div className="bottom"></div>
+    </div>
   )
 }
 
