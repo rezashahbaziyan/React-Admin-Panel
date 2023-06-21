bcaade06c7d6bcc3695c580b2825cce28f37bb9d
ujin <rezashahtrader@gmail.com>
2023-06-21 16:20:46 +0330

    .

---
+++ src/Component/Featured/Featured.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Featured.scss";
+
+const Featured = () => {
+  return (
+    <div className='featured'>Featured</div>
+  )
+}
+
+export default Featured
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.scss";
 import Sidebar from '../../Component/Sidebar/Sidebar';
 import Navbar from '../../Component/Navbar/Navbar';
 import Widget from '../../Component/Widget/Widget';
+import Featured from '../../Component/Featured/Featured';
+import Chart from '../../Component/Chart/Chart';
 
 const Home = () => {
   return (
@@ -15,6 +17,10 @@ const Home = () => {
               <Widget type="order"/>
               <Widget type="earning"/>
               <Widget type="balance"/>
+            </div>
+            <div className="charts">
+              <Featured/>
+              <Chart/>
             </div>
           </div>
     </div>
