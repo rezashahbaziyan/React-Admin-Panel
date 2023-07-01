e768cf2f744e248ec8bc78d8d5735315850d5b9c
ujin <rezashahtrader@gmail.com>
2023-07-02 01:03:47 +0330

    .

--- src/Component/Featured/Featured.jsx
+++ src/Component/Featured/Featured.jsx
@@ -3,6 +3,9 @@ import "./Featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
 
 const Featured = () => {
   return (
@@ -21,6 +24,15 @@ const Featured = () => {
             <p className='desc'>
               Lorem ipsum dolor sit amet consectetur adipisicing.
             </p>
+            <div className="summary">
+              <div className="item">
+                <div className="itemTitle">Target</div>
+                <div className="itemResult">
+                  <KeyboardArrowDownIcon fontSize='small'/>
+                  <div className="resultAmount">12.4k</div>
+                </div>
+              </div>
+            </div>
         </div>
     </div>
   )
