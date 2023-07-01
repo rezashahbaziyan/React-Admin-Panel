c2f736643a193588422e4f3c3801dbb88fa9ab3e
ujin <rezashahtrader@gmail.com>
2023-07-02 01:07:18 +0330

    .

--- src/Component/Featured/Featured.jsx
+++ src/Component/Featured/Featured.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./Featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { CircularProgressbar } from 'react-circular-progressbar';
-import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
+
 
 
 const Featured = () => {
@@ -25,6 +26,20 @@ const Featured = () => {
               Lorem ipsum dolor sit amet consectetur adipisicing.
             </p>
             <div className="summary">
+              <div className="item">
+                <div className="itemTitle">Target</div>
+                <div className="itemResult">
+                  <KeyboardArrowDownIcon fontSize='small'/>
+                  <div className="resultAmount">12.4k</div>
+                </div>
+              </div>
+              <div className="item">
+                <div className="itemTitle">Target</div>
+                <div className="itemResult">
+                  <KeyboardArrowDownIcon fontSize='small'/>
+                  <div className="resultAmount">12.4k</div>
+                </div>
+              </div>
               <div className="item">
                 <div className="itemTitle">Target</div>
                 <div className="itemResult">
