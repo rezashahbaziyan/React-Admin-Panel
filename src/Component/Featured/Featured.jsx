7c0eb375ce80eac357a3b48aa084d2a3b0eeeaaf
ujin <rezashahtrader@gmail.com>
2023-07-02 01:14:52 +0330

    .

--- src/Component/Featured/Featured.jsx
+++ src/Component/Featured/Featured.jsx
@@ -34,14 +34,14 @@ const Featured = () => {
                 </div>
               </div>
               <div className="item">
-                <div className="itemTitle">Target</div>
+                <div className="itemTitle">Last Week</div>
                 <div className="itemResult positive">
                   <KeyboardArrowUpIcon fontSize='small'/>
                   <div className="resultAmount">12.4k</div>
                 </div>
               </div>
               <div className="item">
-                <div className="itemTitle">Target</div>
+                <div className="itemTitle">Last Month</div>
                 <div className="itemResult positive">
                   <KeyboardArrowUpIcon fontSize='small'/>
                   <div className="resultAmount">12.4k</div>
